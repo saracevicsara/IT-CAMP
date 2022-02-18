1134abbbbe3e5ce85359b1e75d1ba511ef4b383c
unknown <sarasaravevic@gmail.com>
2022-02-18 20:26:03 +0100

    prvi zadaci

---
+++ kurs.py
@@ -0,0 +1,64 @@
+# print("sara")
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# print(x)import random
+# import random
+
+# print(random.randrange(1, 10))
+
+#obnavljanje osnovnih tipova podataka i operatora
+# x = int(1)   # x will be 1
+# y = int(2.8) # y will be 2   #tipovi podataka>>>int
+# z = int("3") # z will be 3
+
+
+
+# x = float(1)     # x will be 1.0
+# y = float(2.8)   # y will be 2.8
+# z = float("3")   # z will be 3.0     #ostali tipovi podataka>>>float
+# w = float("4.2") # w will be 4.2
+
+
+
+# x = str("s1") # x will be 's1'
+# y = str(2)    # y will be '2'       #ostalii tipovi podataka>>>string
+# z = str(3.0)  # z will be '3.0'
+
+
+#operatori W3SCHOOLS
+# b = "Hello, World!"
+# print(b[2:5])        #BROJI OD 2 DO 5  0,1,2,3,4,5,6,7,8,9
+# x="sarkica"
+# print(x[1:6])       #BROJEVI SU INDEKSIRANI OD 0
+
+# b = "Hello, World!"
+# print(b[-5:-2])               #From: "o" in "World!" (position -5)
+
+#To, but not included: "d" in "World!" (position -2):
+
+
+# dolari=int(input("Unesi vrednost u dolarima: "))
+# vr_dolara=106.77                                               #INPUT KORISTIMO KAO NAREDBU KOJU UNOSIMO NAKNADNO TJS.U TERMINALU
+# dinari=dolari*vr_dolara
+# print("Uneta vrednost dolara u dinarima iznosi: ", dinari)  
+
+
+# x=input("unesite zeljeni broj")
+# r=x*5lejla
+
+# print(r)
+
+# rec=input("unesite zeljenu rec")
+# x=rec*10           #ako zelimo razmak izmedju reci varijablur ce stavljamo u zagradi dodajemo plus i jednostruke navodnike
+# print(x)
+
+# broj=4
+# brojj=5
+# proizvod=broj*brojj
+# print(proizvod)
+
+# cokolada=100
+# mleko=150
+# cips=60.5
+# torba=cokolada+mleko+cips
+# print(torba)
