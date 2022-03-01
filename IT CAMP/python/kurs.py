b42a10bd946f6ed6af24d35a40065c6dbaf62776
unknown <sarasaravevic@gmail.com>
2022-03-01 22:31:27 +0100

    ...

---
+++ IT CAMP/python/kurs.py
@@ -0,0 +1,32 @@
+# broj="sara" #ovde imamo varijablu i string
+# print(broj)
+
+# broj=input("unesite zeljeni broj")
+# print(broj)
+# OTORINOLARINGOLOGIJA LOGIJA
+
+# 1638 int 
+# broj=7585497
+# print(broj)
+# 8183.986 float
+# broj=7846
+# "khdhueb"
+# "5885685"
+# ime="bdhwjdvnejcb"
+# print(ime)
+# broj=678
+# print(broj)
+# broj="sarsaracevic"
+# print(len(broj))
+# a = 33
+# b = 200
+# if a >b:
+#     print("a nije vece od b")
+# rint(10>5)p
+# a=7
+# print(bool(a))
+# a=0
+# print(bool(a))
+# a,b,c,e,d=int(input("unesite broj"))
+# sredina=(a+b+c+e+d)/5
+# print("ovo je aritmeticka sredina",sredina)
