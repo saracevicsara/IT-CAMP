e13ff24bd93de089492596fabaa0799da918f428
unknown <sarasaravevic@gmail.com>
2022-03-05 02:15:46 +0100

    ...

--- IT CAMP/python/kurs.py
+++ IT CAMP/python/kurs.py
@@ -21,9 +21,38 @@
 # a = 33
 # b = 200
 # if a >b:
+#     print("a je od b")
+# else:
 #     print("a nije vece od b")
 # rint(10>5)p
 # a=7
 # print(bool(a))
 # a=0
 # print(bool(a))
+
+#IF ELIF ELSE
+#GRADI SE NA OSNOVU TRUE I FALSE ,JER BIRAMO IZMEDJU DVA PONUDJENA ODGOVORA
+# x=int(input("unesite prirodan broj"))
+# if x%7==0:#operator za deljenje sa ostatkom jer nismo sigurni da je svaki broj deljiv sa tacno sedam ,onda ostatak uporedjujemo sa nulom
+#     print("broj je deljiv sa sedam")
+# else:
+#     print("broj  nije deljiv sa sedam")
+
+# x=int(input("unesite neki broj"))
+# y=int(input("unesite neki broj"))
+# z=x+y #postavljamo zadatak
+# if z>20:#koristimo dostupne operatore za if 
+#     print("suma je veca od 20")
+# else:
+#     print("suma je manja od 20")
+
+
+
+# x=int(input("unesite random broj"))
+# y=int(input("unesite random broj"))
+# z=x+y
+# s=x*y  #mozemo printati varijablu s i z da bi videli zbir i proizvod
+# if z==s:
+#     print("zbir i proizvod su jednaki")
+# else:
+#     print( "zbir i proizvod su razliciti")
